f1ba6fe1b1d3504adcc38b8a69955d5a33008cdb
huyahrix <infjnite@gmail.com>
2019-10-14 14:12:02 +0700

    db config

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # OMSv1
+
+# install pyodbc
+    #https://docs.microsoft.com/en-us/sql/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server?view=sql-server-ver15
+    RedHat Enterprise Server 7 - for centos 7
--- api/configs/db.py
+++ api/configs/db.py
@@ -10,16 +10,12 @@ username = config_by_name[ENV].DB_USERNAME
 password = config_by_name[ENV].DB_PASSWORD
 connectionstring = config_by_name[ENV].DB_CONNECTIONSTRING
 port = 1433
-# driver = {[x for x in db.drivers() if x.endswith('17 for SQL Server')][0]}
-# driver = db.drivers()
-print("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ")
-for item in db.drivers():
-    print(item)
+# list odbc driver
+# for item in db.drivers():
+#     print(item)
 
-
-# driver = [item for item in db.drivers()][-1]
-driver = "ODBC Driver 17 for SQL Server"
-print(driver)
+# driver = "ODBC Driver 17 for SQL Server"
+driver = "ODBC Driver 13 for SQL Server" # centos 7
 connectionstring = f'DRIVER={driver};SERVER={server};UID={username};PWD={password};DATABASE={database}'
 print(connectionstring)
 
