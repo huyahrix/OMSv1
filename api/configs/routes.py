21ccad249d7e4fbc8e4a40a9dad20c595d0255df
huyahrix <infjnite@gmail.com>
2019-10-15 17:16:08 +0700

    none

--- api/configs/routes.py
+++ api/configs/routes.py
@@ -5,8 +5,8 @@ from api.util.dedault import Default
 def register_routes(api):
     
     api.add_resource(Default,"/")
-    api.add_resource(Login,   "/api/v1/system/login")
-    api.add_resource(Logout,  "/api/v1/system/logout")
-    api.add_resource(Refresh,  "/api/v1/system/refresh")
+    api.add_resource(Login,  "/api/v1/system/login")
+    api.add_resource(Logout, "/api/v1/system/logout")
+    api.add_resource(Refresh,"/api/v1/system/refresh")
 
-    api.add_resource(Menu,    "/api/v1/system/menus")
+    api.add_resource(Menu,   "/api/v1/system/menus")
