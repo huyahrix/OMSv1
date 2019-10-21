8842184e16282557612bd05fa44d2652167b93fc
huyahrix <infjnite@gmail.com>
2019-10-21 14:33:14 +0700

    logging

--- run.py
+++ run.py
@@ -45,6 +45,6 @@ if __name__ != '__main__':
 
 
     gunicorn_logger = logging.getLogger('gunicorn.error')
-    gunicorn_logger.FileHandler('gunicorn.error')
+    # gunicorn_logger.FileHandler('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
