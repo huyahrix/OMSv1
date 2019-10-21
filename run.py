e671ed282e9f9eaeed11d474c4dfe94c9f37116c
huyahrix <infjnite@gmail.com>
2019-10-21 16:15:48 +0700

    logging

--- run.py
+++ run.py
@@ -4,8 +4,10 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager,exceptions
 from api import create_app
 from api.configs.routes import register_routes
-import logging
 from api.util.blacklist_helpers import is_token_revoked
+import logging
+from logging.handlers import RotatingFileHandler
+from logging import Formatter
 
 app = create_app()
 CORS(app)
@@ -23,28 +25,3 @@ register_routes(api)
 
 if __name__ == '__main__':
     app.run()
-
-
-if __name__ != '__main__':
-    print('logging testing')
-    # LOGGER = logging.getLogger('whatever')
-    # file_handler = logging.FileHandler('test.log')
-    # handler = logging.StreamHandler()
-    # LOGGER.addHandler(file_handler)
-    # LOGGER.addHandler(handler)
-    # LOGGER.setLevel(logging.INFO)
-
-
-
-    # logging.basicConfig(filename='error.log',level=logging.DEBUG)
-    # file_handler = logging.handlers.RotatingFileHandler('python.log', maxBytes=1024 * 1024 * 100, backupCount=20)
-    # file_handler.setLevel(logging.ERROR)
-    # formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    # file_handler.setFormatter(formatter)
-    # app.logger.addHandler(file_handler)
-
-
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    # gunicorn_logger.FileHandler('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,10 @@
 # Gunicorn does not support Windows
+import logging
 from run import app 
 
 if __name__ == "__main__":
+    print('run here')
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
     app.run()
