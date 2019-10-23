6bf6195c80881725fc673173b0b08e9fd8d35dbf
root <root@localhost.localdomain>
2019-10-23 08:45:35 +0700

    stativ

---
+++ api/templates/7.a275c234ef2f3f2bda51.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{663:function(n,e,t){"use strict";t.r(e);var a=t(0),u=t.n(a);e.default=function(){return u.a.createElement("div",null,"News Page !")}}}]);
