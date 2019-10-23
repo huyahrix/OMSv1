5277183a0789374e5d5eba1b5fae8d0585de3dd9
huyahrix <infjnite@gmail.com>
2019-10-23 16:01:07 +0700

    serve webpack

--- api/configs/routes.py
+++ api/configs/routes.py
@@ -3,10 +3,19 @@ from api.modules.systems.menus import Menu
 from api.modules.serve.serve import Serve_static, Default
 
 def register_routes(api):
-    
-    api.add_resource(Default,      "/")
-    api.add_resource(Serve_static, "/static/<path:filename>")
-    api.add_resource(Login,        "/api/v1/system/login")
-    api.add_resource(Logout,       "/api/v1/system/logout")
-    api.add_resource(Refresh,      "/api/v1/system/refresh")
-    api.add_resource(Menu,         "/api/v1/system/menus")
+
+    # Serving static files
+    api.add_resource(Default,              "/")
+    routes = [
+        '/<string:filename>', # webpack file
+        '/public/images/<string:filename>', # webpack file
+        '/static/<path:filename>',
+        ]
+    api.add_resource(Serve_static,         *routes)
+
+    # auth
+    api.add_resource(Login,                "/api/v1/system/login")
+    api.add_resource(Logout,               "/api/v1/system/logout")
+    api.add_resource(Refresh,              "/api/v1/system/refresh")
+
+    api.add_resource(Menu,                 "/api/v1/system/menus")
--- api/modules/serve/serve.py
+++ api/modules/serve/serve.py
@@ -19,38 +19,27 @@ import os
 
 
 class Serve_static(Resource):
-    #@jwt_required
     def get(self,filename):
-        current_app.logger.debug('serve : %f',filename) 
         path = current_app.config.get('STATIC_FOLDER')
-        if 'js/' in filename:
+        if '.js' in filename:
             if current_app.config.get('OS_WIN'):
-                path = current_app.config['STATIC_FOLDER'] + '\\js1'
+                path = current_app.config['STATIC_FOLDER'] + '\\js'
             else:
                 path = current_app.config['STATIC_FOLDER'] + '/js'
             filename = filename.replace('js/','')
+        if '.svg' in filename:
+            if current_app.config.get('OS_WIN'):
+                path = current_app.config['STATIC_FOLDER'] + '\\images'
+            else:
+                path = current_app.config['STATIC_FOLDER'] + '/images'
+            filename = filename.replace('images/','')
+
         if not os.path.exists(path + ('\\','/')[current_app.config.get('OS_WIN')] + filename):
             return make_response(jsonify(status=404, msg="Cannot find <id={filename}> in {path}".format(filename=filename, path=path)),404)
         return send_from_directory(path,filename)
-
-        # STATIC_FOLDER = 'd:\\Privite\\Python\\OMSv1\\api\\static'
-        # path = current_app.config['STATIC_FOLDER'] + filename
-        # #return send_from_directory(current_app.config['STATIC_FOLDER'], 'index.html')
-        # return send_from_directory('d:\\Privite\\Python\\OMSv1\\api\\static\\js','runtime.912487c9b6fba62d63dc.js')
 
 
 class Default(Resource):
     def get(self):
         headers = {'Content-Type': 'text/html'}
-        # if not os.path.exists(path + '\\' + filename):
-        #     return make_response(jsonify(status=status.HTTP_200_OK, msg="Cannot find <id={filename}> in {path}".format(filename=filename, path=path)),404)
         return make_response(render_template('index.html'),200,headers)
-
-
-class Render(Resource):
-    def get(self):
-         message = "Hello, World"
-         headers = {'Content-Type': 'text/html'}
-         path = current_app.config['TEMPLATES_FOLDER'] + '\\render.html'
-         return make_response(render_template('render.html',message=message),200,headers)
-         #return render_template('index.html',message=message)
